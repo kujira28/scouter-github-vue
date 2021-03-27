7cd012aba240c4a2de310642da7779efa7869115
先規　勇樹 <yukisaki80@gmail.com>
2021-03-27 21:46:42 +0900

    ドラッグ＆ドロップ機能

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,7 @@
   <div class="bg-gray-100 pb-60">
       <div class="max-w-l mx-auto p-1 pt-10 pr-0 text-center">
         <h2 class="text-lg font-bold text-gray-800">{{ displayMonth }}</h2>
-        <form
-          class="flex justify-center items-center my-10"
-          v-on:submit.prevent="jumpCalendar"
-        >
-          <input
-            class="bg-purple-white shadow rounded border-0 p-2 mr-4"
-            type="text"
-            v-model="jumpYear"
-          />
-          <p class="mr-4 text-lg font-bold text-gray-800">年</p>
-          <input
-            class="bg-purple-white shadow rounded border-0 p-2 mr-4"
-            type="text"
-            v-model="jumpMonth"
-          />
-          <p class="mr-4 text-lg font-bold text-gray-800">月</p>
-
-          <button
-            class="focus:outline-none text-white text-sm py-2 px-5 rounded-md bg-blue-500 hover:bg-blue-600 hover:shadow-lg"
-            type="submit"
-          >
-            移動
-          </button>
-        </form>
         <div class="button-area">
-          <button
-            class="bg-indigo-500 hover:bg-indigo-600 text-white py-2 px-4 text-xs ml-4 mb-4 mr-8 ml-auto"
-            @click="prevMonth"
-          >
-            前の月
-          </button>
-          <button
-            class="bg-indigo-500 hover:bg-indigo-600 text-white py-2 px-4 text-xs ml-4 mb-4 mr-0 ml-auto"
-            @click="nextMonth"
-          >
-            次の月
-          </button>
         </div>
         <button
           @click="addTask"
@@ -276,12 +240,6 @@ export default {
   }
   },
          methods: {
-          jumpCalendar() {
-            this.currentDate = moment(this.currentDate)
-              .year(parseInt(this.jumpYear))
-              .month(parseInt(this.jumpMonth))
-              .subtract(1, 'month');
-          },
           addTask() {
             this.update_mode = false;
             this.form = {};
@@ -447,13 +405,6 @@ export default {
             }
           },
 
-          nextMonth() {
-            this.currentDate = moment(this.currentDate).add(1, 'month');
-          },
-          prevMonth() {
-            this.currentDate = moment(this.currentDate).subtract(1, 'month');
-          },
-
           youbi(dayIndex) {
             let week = ['日', '月', '火', '水', '木', '金', '土'];
             const slice = week.splice(0, this.weekSlice);
@@ -461,82 +412,14 @@ export default {
             return week[dayIndex];
           },
 
-          dragStart(dayEvent) {
-            event.dataTransfer.effectAllowed = 'move';
-            event.dataTransfer.dropEffect = 'move';
-            event.dataTransfer.setData('eventId', dayEvent);
+          dragStart() {
           },
 
-          dragleft(dayEvent) {
-            event.dataTransfer.effectAllowed = 'move';
-            event.dataTransfer.dropEffect = 'move';
-            event.dataTransfer.setData('dragIdLeft', dayEvent);
+          dragleft() {
           },
-          dragright(dayEvent) {
-            event.dataTransfer.effectAllowed = 'move';
-            event.dataTransfer.dropEffect = 'move';
-            event.dataTransfer.setData('dragIdRight', dayEvent);
+          dragright() {
           },
-          dragEnd(date) {
-            let eventId = event.dataTransfer.getData('eventId');
-            let dragIdLeft = event.dataTransfer.getData('dragIdLeft');
-            let dragIdRight = event.dataTransfer.getData('dragIdRight');
-            // this.colRight = false;
-
-            if (eventId !== '') {
-              let dragEvent = this((event) => event == eventId);
-              let betweenDays = moment(
-                moment(dragEvent),
-                'days'
-              );
-              dragEvent.start = date;
-              dragEvent.end = moment(dragEvent.start)
-                .add(betweenDays, '')
-                .format('YYYY-MM-DD');
-
-              let drag_index;
-              this((task, index) => {
-                if (task === parseFloat(eventId)) {
-                  drag_index = index;
-                }
-              });
-              this(drag_index, 1);
-              this();
-
-            }
-
-            if (dragIdLeft !== '') {
-              let dragEvent = this(
-                (event) => event == dragIdLeft
-              );
-              dragEvent.start = date;
-
-              let drag_index;
-              this((task, index) => {
-                if (task === parseFloat(dragIdLeft)) {
-                  drag_index = index;
-                }
-              });
-              this.events.splice(drag_index, 1);
-              this.events.push(dragEvent);
-            }
-
-            if (dragIdRight !== '') {
-              let dragEvent = this(
-                (event) => event == dragIdRight
-              );
-              dragEvent = date;
-
-              let drag_index;
-              this((task, index) => {
-                if (task === parseFloat()) {
-                  drag_index = index;
-                }
-              });
-              this.events(drag_index, 1);
-              this.events(dragEvent);
-              this.colRight = true;
-            }
+          dragEnd() {
           },
         },
         computed: {
@@ -550,38 +433,10 @@ export default {
             return this.currentDate.format('YYYY-MM');
           },
           sortedEvents() {
-            return this.events.slice().sort(function (a, b) {
-              let startTime = parseInt(a.startTime.replace(':', ''));
-              let endTime = parseInt(b.startTime.replace(':', ''));
-
-              if (startTime < endTime) return -1;
-              if (startTime > endTime) return 1;
-              return 0;
+            return this.events.slice(function () {
             });
           },
 
-          sortedEventsByHour() {
-            return this.events.slice().sort(function (a, b) {
-              if (
-                moment(a.end).diff(moment(a.start), 'days') === 1 &&
-                moment(b.end).diff(moment(b.start), 'days') === 1
-              ) {
-                let startTime = moment(a.startTime).format('h:mm');
-                let endTime = moment(b.startTime).format('h:mm');
-                if (startTime < endTime) return -1;
-                if (startTime > endTime) return 1;
-                return 0;
-              } else {
-                return this.events.slice().sort(function (a, b) {
-                  let startDate = moment(a.start).format('YYYY-MM-DD');
-                  let endDate = moment(b.start).format('YYYY-MM-DD');
-                  if (startDate < endDate) return -1;
-                  if (startDate > endDate) return 1;
-                  return 0;
-                });
-              }
-            });
-          },
         },
 }
 </script>
